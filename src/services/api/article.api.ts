18aae7b086bd0550a6a46012299ae36b43d3c9df
Amirhossein Douzandeh <amirzenoozi72@gmail.com>
2021-08-19 20:04:13 +0430

    :zap: add new api about single article

--- src/services/api/article.api.ts
+++ src/services/api/article.api.ts
@@ -9,6 +9,41 @@ const getArticles = (queryParams?: any): Observable<any> => {
   }) as Observable<any>;
 };
 
+const getArticleTags = (queryParams?: any): Observable<any> => {
+  return api.get<any>({
+    url: 'api/tags',
+    shouldAuth: true,
+    queryParams,
+  }) as Observable<any>;
+};
+
+const createNewArticle = (formFields: any): Observable<any> => {
+  return api.post<any>({
+    url: 'api/articles',
+    shouldAuth: true,
+    body: formFields,
+  }) as Observable<any>;
+};
+
+const updateArticleBySlug = (slug: string, formFields: any): Observable<any> => {
+  return api.put<any>({
+    url: `api/articles/${slug}`,
+    shouldAuth: true,
+    body: formFields,
+  }) as Observable<any>;
+};
+
+const getArticleBySlug = (slug: string): Observable<any> => {
+  return api.get<any>({
+    url: `api/articles/${slug}`,
+    shouldAuth: true,
+  }) as Observable<any>;
+};
+
 export {
   getArticles,
+  getArticleTags,
+  createNewArticle,
+  updateArticleBySlug,
+  getArticleBySlug,
 };
