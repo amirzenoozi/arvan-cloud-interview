229d817c563ad3b02869feef8d7d32a48574c10e
Amirhossein Douzandeh <amirzenoozi72@gmail.com>
2021-08-19 17:44:21 +0430

    :zap: update routes

--- src/layout/sidebar/menus.seed.ts
+++ src/layout/sidebar/menus.seed.ts
@@ -1,11 +1,17 @@
 import { RouteName } from 'src/pages/dashboard/dashborad.routes';
 import { IMenuItem } from './menus.interface';
 import { Dashboard } from '@material-ui/icons';
+import { NoteAdd } from '@material-ui/icons';
 
 export const menus: IMenuItem[] = [
   {
     label: 'sidebar.dashboard',
     icon: Dashboard,
     path: RouteName.MAIN,
+  },
+  {
+    label: 'sidebar.addArticle',
+    icon: NoteAdd,
+    path: RouteName.ARTICLES_CREATE,
   },
 ];
--- src/pages/dashboard/dashborad.routes.tsx
+++ src/pages/dashboard/dashborad.routes.tsx
@@ -1,9 +1,12 @@
 import React, { lazy, LazyExoticComponent } from 'react';
 
 const MainPage = lazy(() => import('src/pages/dashboard/main'));
+const AddEditPage = lazy(() => import('src/pages/dashboard/add-edit-article'));
 
 export enum RouteName {
   MAIN = 'articles',
+  ARTICLES_CREATE = 'articles/create',
+  ARTICLES_EDIT = 'articles/edit/:slug',
   ARTICLES_PAGES = 'articles/page/:page',
 }
 
@@ -25,6 +28,18 @@ export const routes: RouteProps[] = [
   {
     path: RouteName.ARTICLES_PAGES,
     component: MainPage,
+    routeTitle: 'articles',
+    exact: true,
+  },
+  {
+    path: RouteName.ARTICLES_CREATE,
+    component: AddEditPage,
+    routeTitle: 'articles',
+    exact: true,
+  },
+  {
+    path: RouteName.ARTICLES_EDIT,
+    component: AddEditPage,
     routeTitle: 'articles',
     exact: true,
   },
